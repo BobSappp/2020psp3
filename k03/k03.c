d0b44bd3b115888222a248e27a241a8b9f3d5a15
muraya <bb35319048@ms.nagasaki-u.ac.jp>
2020-12-13 18:56:23 +0900

    お願いします

--- k03/k03.c
+++ k03/k03.c
@@ -10,13 +10,72 @@ char StrKey[] = "wind";
 char* ForceSearch(char text[], char key[])
 {
     //  ここを実装する
+    int start=0, pos=0,i,key_len;
+    
+    for(i=0;key[i]!='\0';i++){
+        key_len = i;
+    }
+while(text[pos+start]!='\0'){
+    if(text[start+pos]==key[pos]){
+        pos++;
+        if(pos=key_len+1){
+            return(&text[start]);
+            break;
+        }
+    }else{
+        pos=0;
+        start++;
 
+    }
+    }
+    if(text[pos+start]=='\0'){
+        return(NULL);
+    }
 }
+
 
 char* BMSearch(char text[], char key[])
 {
     //  ここを実装する
+    int key_len=0,text_len=0;
+    int i,zurashi,a,b,table[255];
+    for(i=0;key[i]!='\0';i++){
+        text_len = i;
+    }
 
+    for(i=0;key[i]!='\0';i++){
+        key_len = i;
+    }
+//key_len=3?
+    for(i=0;key[i]<=255;i++){
+        table[i]=key_len+1;
+    }
+
+    for(i=0;key[i]!='\0';i++){
+        table[(unsigned char)key[i]]=key_len-i;
+    }
+    
+    for(i=key_len;i<=text_len;i=a+zurashi){
+        for(a=i;a>=a-key_len;a--){
+            if(text[a]==key[key_len-b]){
+                b++;
+                if(b==key_len+1){
+                    return(&text[i-key_len]);
+
+                }
+                else{
+                    b=0;
+                    zurashi=table[(unsigned char)text[a]];
+                    if(a+zurashi<=i){
+                        zurashi=i+1-a;
+                    }
+                    break;
+                }
+
+            }
+        }
+    }    
+    return NULL;
 }
 
 int main(void)
